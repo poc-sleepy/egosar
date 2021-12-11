510d27a5c759f635f1bfc081cf2db942b2560bdc
poc-sleepy <sleepy.sleepy.days@gmail.com>
2021-12-11 11:11:58 +0900

    ✨ 適当にSlackのWebhookを叩く処理を追加

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import TwitterApi from 'twitter-api-v2';
-import { twitterKey } from './settings';
+import { twitterKey, slackWebhookUrl } from './settings';
+import { IncomingWebhook, IncomingWebhookSendArguments } from '@slack/webhook';
 
 // Instanciate with desired auth type (here's Bearer v2 auth)
 const twitterClient = new TwitterApi(twitterKey);
 // Tell typescript it's a readonly app
 const roClient = twitterClient.readOnly;
 
+const slackTest = async () => {
+  const slackMessage = new IncomingWebhook(slackWebhookUrl);
+  const slackMessageContent: IncomingWebhookSendArguments = {
+    attachments: [
+      {
+        color: '#1976d2',
+        text: 'これはSlack投稿のテスト',
+      },
+    ],
+  };
+
+  return await slackMessage.send(slackMessageContent);
+};
+
 const handler = async () => {
   // Play with the built in methods
   const user = await roClient.v2.userByUsername('__poc_sleepy__');
   console.log(user);
+  console.log(slackTest());
 };
 
 handler();
--- src/settings.ts
+++ src/settings.ts
@@ -4,3 +4,8 @@ if (process.env.TWITTER_KEY === undefined) {
   throw Error('TWITTER_KEY is not found');
 }
 export const twitterKey = process.env.TWITTER_KEY;
+
+if (process.env.SLACK_WEBHOOK_URL === undefined) {
+  throw Error('SLACK_WEBHOOK_URL is not found');
+}
+export const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
